7e1da37a83af11994008f016130159b50a7ee598
anasib <atta.nasib@bytecubed.com>
2018-03-06 15:23:45 -0500

    hangman code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,95 @@
-var randomwWordArr = ["Brad Pitt", "Tom Cruise", "Al Pacino", "Tom Hanks", "Robert DeNiro",];
-var randomWord = randomwWordArr[Math.floor(Math.random() * randomwWordArr.length)];
 
-var count = 0;
-var answerArr = [];
-var s;
-var alphabetArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+//actors name to guess
+var randomActorArr = ["brad pitt", "tom cruise", "al pacino", "tom hanks", "robert deniro"];
+//randomly chosen actor
+var randomWord = randomActorArr[Math.floor(Math.random() * randomActorArr.length)];
 
+var correctLetter = [];
+var wrongLetters = [];
+var dashes = [];
 
-var guessLeft = 9;
-var win = 0;
-var losses = 0;
-var letterUse = [];
+var wins = 0;
+var loss = 0;
+var guessesLeft = 12;
 
-function countGuessesLeft () {
-    document.querySelector("#remainingGuess").innerHTML = "Number of Guesses Remaining: " + guessLeft;
-}
+//choose random words
+var lettersInWord = randomWord.split('');
+console.log(lettersInWord);
 
+//actors names
+var correctGuess = document.getElementById('correct');
+//correct letters
+var wrongGuess = document.getElementById('wrong');
+//wrong letters
+var remainigLetters = document.getElementById('remainingGuess');
 
+var actor1 = document.getElementById('alpacino');
+var actor2 = document.getElementById('bradPitt');
+var actor3 = document.getElementById('tomCruise');
+var actor4 = document.getElementById('tomHanks');
+var actor5 = document.getElementById('robertDeniro');
+
+//js code below
+
+$(document).ready(function () {
+
+    underscore = function () {
+        for (var i = 0; i < randomWord.length; i++) {
+            dashes.push('_');
+        }
+        return dashes;
+    }
+    underscore();
+
+    document.onkeyup = function (event) {
+        var key = event.key;
+
+        for (var i = 0; i = randomWord.length; i++) {
+
+            if (correctLetter.indexOf(key) !== -1) {
+                return false;
+            }
+
+            if (wrongLetters.indexOf(key) !== -1) {
+                return false;
+            }
+
+            function restartGame() {
+                location.reload();
+            }
+
+            sessionStorage.setItem("wins", wins);
+            sessionStorage.setItem("lose", loss);
+
+            correctLetter.push(key);
+            dashes.push(key);
+            correctGuess.innerText = "Correct Letters: " + dashes.join(' ');
+            wrongGuess.innerText = "Wrong Guesses: " + correctLetter.join(' ');
+
+            if (randomWord.indexOf(key) > -1) {
+                correctLetter.push(' ' + key);
+                dashes[randomWord.indexOf(key)] = key;
+
+                if (dashes.join(' ') === randomWord) {
+                    alert("Winner!!!");
+                    return restartGame();
+                }
+
+            } else {
+                wrongLetters.push(' ' + key);
+                dashes[randomWord.indexOf(key)] = key;
+                remainigLetters[0].innerHTML = wrongLetters;
+                guessesLeft--;
+
+                if (guessesLeft == 0) {
+                    alert("Loser!!!");
+                    return restartGame();
+                }
+            }
+
+            document.getElementById("remainingGuess").innerHTML = guessesLeft;
+
+        }
+    }
+
+});
